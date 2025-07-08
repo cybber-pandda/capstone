0e3bc0e6417e0e744107d5aae6e50373fccb746c
arvin-macalalad <arvinmacalalad807@gmail.com>
2025-07-08 09:27:33 +0000

    add profile page

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,5 +1,112 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <div class="page-content container-xxl"></div>
+    <div class="page-content container-xxl">
+
+        <div class="row">
+            <div class="col-12 grid-margin">
+                <div class="card">
+                    <div class="position-relative">
+                        <figure class="overflow-hidden mb-0 d-flex justify-content-center">
+                            <img src="../../../assets/images/others/profile-cover.jpg" class="rounded-top"
+                                alt="profile cover">
+                        </figure>
+                        <div
+                            class="d-flex justify-content-between align-items-center position-absolute top-90 w-100 px-2 px-md-4 mt-n4">
+                            <div>
+                                <img class="w-70px rounded-circle" src="../../../assets/images/faces/face1.jpg"
+                                    alt="profile">
+                                <span class="h4 ms-3 text-dark">Amiah Burton</span>
+                            </div>
+                            <div class="d-none d-md-block">
+                                <button class="btn btn-primary btn-icon-text">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" data-lucide="edit"
+                                        class="lucide lucide-edit btn-icon-prepend">
+                                        <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                        <path
+                                            d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z">
+                                        </path>
+                                    </svg> Edit profile
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row profile-body">
+            <!-- left wrapper start -->
+            <div class="d-none d-md-block col-12 left-wrapper">
+                <div class="card rounded">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between mb-2">
+                            <h6 class="card-title mb-0">About</h6>
+                            <div class="dropdown">
+                                <a type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" data-lucide="more-horizontal"
+                                        class="lucide lucide-more-horizontal icon-lg text-secondary pb-3px">
+                                        <circle cx="12" cy="12" r="1"></circle>
+                                        <circle cx="19" cy="12" r="1"></circle>
+                                        <circle cx="5" cy="12" r="1"></circle>
+                                    </svg>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;"><svg
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                            stroke-linejoin="round" data-lucide="edit-2"
+                                            class="lucide lucide-edit-2 icon-sm me-2">
+                                            <path
+                                                d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z">
+                                            </path>
+                                        </svg> <span class="">Edit</span></a>
+                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;"><svg
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                            stroke-linejoin="round" data-lucide="git-branch"
+                                            class="lucide lucide-git-branch icon-sm me-2">
+                                            <line x1="6" x2="6" y1="3" y2="15"></line>
+                                            <circle cx="18" cy="6" r="3"></circle>
+                                            <circle cx="6" cy="18" r="3"></circle>
+                                            <path d="M18 9a9 9 0 0 1-9 9"></path>
+                                        </svg> <span class="">Update</span></a>
+                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;"><svg
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                            stroke-linejoin="round" data-lucide="eye"
+                                            class="lucide lucide-eye icon-sm me-2">
+                                            <path
+                                                d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0">
+                                            </path>
+                                            <circle cx="12" cy="12" r="3"></circle>
+                                        </svg> <span class="">View all</span></a>
+                                </div>
+                            </div>
+                        </div>
+                        <p>Hi! I'm Amiah the Senior UI Designer at NobleUI. We hope you enjoy the design and quality of
+                            Social.</p>
+                        <div class="mt-3">
+                            <label class="fs-11px fw-bolder mb-0 text-uppercase">Joined:</label>
+                            <p class="text-secondary">November 15, 2015</p>
+                        </div>
+                        <div class="mt-3">
+                            <label class="fs-11px fw-bolder mb-0 text-uppercase">Username:</label>
+                            <p class="text-secondary">Renzo</p>
+                        </div>
+                        <div class="mt-3">
+                            <label class="fs-11px fw-bolder mb-0 text-uppercase">Email:</label>
+                            <p class="text-secondary">me@nobleui.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- left wrapper end -->
+        </div>
+
+    </div>
 @endsection
